f36fbdfa0af3052fc6faa0d2c4d51e88f5cd4ac4
stannardtyler <tstannard64@gmail.com>
2024-12-06 17:22:17 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,18 +2,25 @@ let mic;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
+
   mic = new p5.AudioIn();
   mic.start();
+  background(0);
 }
 
 function draw() {
-  background(0);
+  noFill();
+  strokeWeight(2);
+  stroke(255);
   
-  let vol = mic.getLevel()
-  circle(width/2, height/2, vol*width*3)
+  background(0, 10);
+
+  let vol = mic.getLevel();
+  circle(width / 2, height / 2, vol * width * 3);
 }
 
 function mousePressed() {
   userStartAudio();
+
+  ellipse(random(width), random(height), 20, 20);
 }
